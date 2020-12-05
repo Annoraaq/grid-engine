89b73ffc9f55357459de7f016996b3ec4541ea6c
Johannes Baum <baum@bw-webgestaltung.de>
2020-12-05 14:20:04 +0100

    add next test

--- src/GridCharacter/GridCharacter.test.ts
+++ src/GridCharacter/GridCharacter.test.ts
@@ -21,15 +21,24 @@ describe("GridCharacter", () => {
 
   it("should initialize with standing face down frame", () => {
     const charIndex = 3;
-    gridCharacter = new GridCharacter(<any>spriteMock, charIndex, 16);
+    gridCharacter = new GridCharacter(spriteMock, charIndex, 16);
     expect(spriteMock.setFrame).toHaveBeenCalledWith(37);
   });
 
   it("should get position", () => {
     const spriteCenterPos = new Phaser.Math.Vector2(3, 4);
     spriteMock.getCenter = jest.fn().mockReturnValue(spriteCenterPos);
-    gridCharacter = new GridCharacter(<any>spriteMock, 3, 16);
+    gridCharacter = new GridCharacter(spriteMock, 3, 16);
 
     expect(gridCharacter.getPosition()).toEqual(spriteCenterPos);
+  });
+
+  it("should set tile position", () => {
+    spriteMock.setPosition = jest.fn();
+    gridCharacter = new GridCharacter(spriteMock, 3, 16);
+
+    gridCharacter.setPosition(new Phaser.Math.Vector2(3, 4));
+
+    expect(spriteMock.setPosition).toHaveBeenCalledWith(3, 4);
   });
 });
