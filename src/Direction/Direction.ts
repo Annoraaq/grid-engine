f5fb76006344338f09a51689a3e7c3827a4a8fae
Johannes Baum <baum@bw-webgestaltung.de>
2020-12-30 18:07:19 +0100

    #5: introduce radius movement

--- src/Direction/Direction.ts
+++ src/Direction/Direction.ts
@@ -1,3 +1,8 @@
+import * as Phaser from "phaser";
+
+const Vector2 = Phaser.Math.Vector2;
+type Vector2 = Phaser.Math.Vector2;
+
 export enum Direction {
   NONE = "none",
   LEFT = "left",
@@ -5,3 +10,13 @@ export enum Direction {
   RIGHT = "right",
   DOWN = "down",
 }
+
+export const DirectionVectors: {
+  [key in Direction]?: Vector2;
+} = {
+  [Direction.UP]: Vector2.UP,
+  [Direction.DOWN]: Vector2.DOWN,
+  [Direction.LEFT]: Vector2.LEFT,
+  [Direction.RIGHT]: Vector2.RIGHT,
+  [Direction.NONE]: Vector2.ZERO,
+};
