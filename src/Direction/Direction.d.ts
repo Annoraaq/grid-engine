47a2d94d567d2ca97529412220fcdc9b41e65d3f
Skylear Johnson <sky@surf.gg>
2021-03-14 11:25:32 -0500

    Add prettier, add dev script to prettify

--- src/Direction/Direction.d.ts
+++ src/Direction/Direction.d.ts
@@ -2,14 +2,14 @@ import * as Phaser from "phaser";
 declare const Vector2: typeof Phaser.Math.Vector2;
 declare type Vector2 = Phaser.Math.Vector2;
 export declare enum Direction {
-    NONE = "none",
-    LEFT = "left",
-    UP = "up",
-    RIGHT = "right",
-    DOWN = "down"
+  NONE = "none",
+  LEFT = "left",
+  UP = "up",
+  RIGHT = "right",
+  DOWN = "down",
 }
 export declare const DirectionVectors: {
-    [key in Direction]?: Vector2;
+  [key in Direction]?: Vector2;
 };
 export declare function oppositeDirection(direction: Direction): Direction;
 export {};
