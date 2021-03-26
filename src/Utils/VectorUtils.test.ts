eb433084cbc77221dc5aaeedc7e3839cfd154c03
Johannes Baum <baum@bw-webgestaltung.de>
2021-03-26 16:29:44 +0100

    #82: add missing test

--- src/Utils/VectorUtils.test.ts
+++ src/Utils/VectorUtils.test.ts
@@ -1,24 +1,28 @@
 import * as Phaser from "phaser";
 import { VectorUtils } from "./VectorUtils";
+
+type Vector2 = Phaser.Math.Vector2;
+const Vector2 = Phaser.Math.Vector2;
+
 describe("VectorUtils", () => {
   describe("vec2str", () => {
     it("should transform a vector into a string", () => {
-      const vector = new Phaser.Math.Vector2(3, -1);
+      const vector = new Vector2(3, -1);
       expect(VectorUtils.vec2str(vector)).toEqual("3#-1");
     });
   });
 
   describe("equal", () => {
     it("should detect equal vectors", () => {
-      const vector = new Phaser.Math.Vector2(3, -1);
-      const vector2 = new Phaser.Math.Vector2(3, -1);
+      const vector = new Vector2(3, -1);
+      const vector2 = new Vector2(3, -1);
       expect(VectorUtils.equal(vector, vector2)).toBe(true);
       expect(VectorUtils.equal(vector2, vector)).toBe(true);
     });
 
     it("should detect unequal vectors", () => {
-      const vector = new Phaser.Math.Vector2(3, -1);
-      const vector2 = new Phaser.Math.Vector2(2, 1);
+      const vector = new Vector2(3, -1);
+      const vector2 = new Vector2(2, 1);
       expect(VectorUtils.equal(vector, vector2)).toBe(false);
       expect(VectorUtils.equal(vector2, vector)).toBe(false);
     });
@@ -26,21 +30,32 @@ describe("VectorUtils", () => {
 
   describe("manhattanDistance", () => {
     it("should correctly calc positive numbers", () => {
-      const vector = new Phaser.Math.Vector2(5, 6);
-      const vector2 = new Phaser.Math.Vector2(2, 3);
+      const vector = new Vector2(5, 6);
+      const vector2 = new Vector2(2, 3);
       expect(VectorUtils.manhattanDistance(vector, vector2)).toEqual(6);
     });
 
     it("should correctly calc negative numbers", () => {
-      const vector = new Phaser.Math.Vector2(-5, -6);
-      const vector2 = new Phaser.Math.Vector2(-2, -3);
+      const vector = new Vector2(-5, -6);
+      const vector2 = new Vector2(-2, -3);
       expect(VectorUtils.manhattanDistance(vector, vector2)).toEqual(6);
     });
 
     it("should correctly calc zero distance", () => {
-      const vector = new Phaser.Math.Vector2(2, 3);
-      const vector2 = new Phaser.Math.Vector2(2, 3);
+      const vector = new Vector2(2, 3);
+      const vector2 = new Vector2(2, 3);
       expect(VectorUtils.manhattanDistance(vector, vector2)).toEqual(0);
+    });
+  });
+
+  describe("scalarMult", () => {
+    it("should multiply immutably", () => {
+      const vector = new Phaser.Math.Vector2(5, 6);
+      expect(VectorUtils.scalarMult(vector, 1)).toEqual(new Vector2(5, 6));
+      expect(VectorUtils.scalarMult(vector, 2)).toEqual(new Vector2(10, 12));
+      expect(VectorUtils.scalarMult(vector, 0)).toEqual(new Vector2(0, 0));
+      expect(VectorUtils.scalarMult(vector, -1)).toEqual(new Vector2(-5, -6));
+      expect(vector).toEqual(new Vector2(5, 6));
     });
   });
 });
