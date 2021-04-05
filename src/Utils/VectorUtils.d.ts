18feed1ce6d0ec0eefcd54f49e061ec7bd3ff20e
Johannes Baum <baum@bw-webgestaltung.de>
2021-04-05 18:08:41 +0200

    ignore declarations

--- src/Utils/VectorUtils.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Phaser from "phaser";
-declare type Vector2 = Phaser.Math.Vector2;
-export declare class VectorUtils {
-    static vec2str(vec: Vector2): string;
-    static equal(vec1: Vector2, vec2: Vector2): boolean;
-    static manhattanDistance(pos1: Vector2, pos2: Vector2): number;
-}
-export {};
