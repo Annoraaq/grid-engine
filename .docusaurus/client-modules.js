fd61ca2f2bbf116fd1bf7e298c82c9dd4f129181
Splashsky <sky@oceanpotion.cc>
2021-07-12 07:14:09 -0500

    Rollback accidental push

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\skyle\\Projects\\ge-docs\\src\\css\\custom.css"),
+];
---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,311 @@
+export default {
+  "title": "Grid Engine",
+  "tagline": "An exceptional plugin for grid-based features in the Phaser 3 engine.",
+  "url": "https://your-docusaurus-test-site.com",
+  "baseUrl": "/",
+  "onBrokenLinks": "throw",
+  "onBrokenMarkdownLinks": "warn",
+  "favicon": "img/favicon.ico",
+  "organizationName": "facebook",
+  "projectName": "docusaurus",
+  "themeConfig": {
+    "navbar": {
+      "logo": {
+        "alt": "Grid Engine",
+        "src": "img/logo.svg"
+      },
+      "items": [
+        {
+          "href": "https://github.com/facebook/docusaurus",
+          "label": "GitHub",
+          "position": "right"
+        }
+      ],
+      "hideOnScroll": false
+    },
+    "footer": {
+      "style": "dark",
+      "links": [],
+      "copyright": "Copyright © 2021 Annoraaq. Built with Docusaurus."
+    },
+    "prism": {
+      "theme": {
+        "plain": {
+          "color": "#393A34",
+          "backgroundColor": "#f6f8fa"
+        },
+        "styles": [
+          {
+            "types": [
+              "comment",
+              "prolog",
+              "doctype",
+              "cdata"
+            ],
+            "style": {
+              "color": "#999988",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "namespace"
+            ],
+            "style": {
+              "opacity": 0.7
+            }
+          },
+          {
+            "types": [
+              "string",
+              "attr-value"
+            ],
+            "style": {
+              "color": "#e3116c"
+            }
+          },
+          {
+            "types": [
+              "punctuation",
+              "operator"
+            ],
+            "style": {
+              "color": "#393A34"
+            }
+          },
+          {
+            "types": [
+              "entity",
+              "url",
+              "symbol",
+              "number",
+              "boolean",
+              "variable",
+              "constant",
+              "property",
+              "regex",
+              "inserted"
+            ],
+            "style": {
+              "color": "#36acaa"
+            }
+          },
+          {
+            "types": [
+              "atrule",
+              "keyword",
+              "attr-name",
+              "selector"
+            ],
+            "style": {
+              "color": "#00a4db"
+            }
+          },
+          {
+            "types": [
+              "function",
+              "deleted",
+              "tag"
+            ],
+            "style": {
+              "color": "#d73a49"
+            }
+          },
+          {
+            "types": [
+              "function-variable"
+            ],
+            "style": {
+              "color": "#6f42c1"
+            }
+          },
+          {
+            "types": [
+              "tag",
+              "selector",
+              "keyword"
+            ],
+            "style": {
+              "color": "#00009f"
+            }
+          }
+        ]
+      },
+      "darkTheme": {
+        "plain": {
+          "color": "#bfc7d5",
+          "backgroundColor": "#292d3e"
+        },
+        "styles": [
+          {
+            "types": [
+              "comment"
+            ],
+            "style": {
+              "color": "rgb(105, 112, 152)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "string",
+              "inserted"
+            ],
+            "style": {
+              "color": "rgb(195, 232, 141)"
+            }
+          },
+          {
+            "types": [
+              "number"
+            ],
+            "style": {
+              "color": "rgb(247, 140, 108)"
+            }
+          },
+          {
+            "types": [
+              "builtin",
+              "char",
+              "constant",
+              "function"
+            ],
+            "style": {
+              "color": "rgb(130, 170, 255)"
+            }
+          },
+          {
+            "types": [
+              "punctuation",
+              "selector"
+            ],
+            "style": {
+              "color": "rgb(199, 146, 234)"
+            }
+          },
+          {
+            "types": [
+              "variable"
+            ],
+            "style": {
+              "color": "rgb(191, 199, 213)"
+            }
+          },
+          {
+            "types": [
+              "class-name",
+              "attr-name"
+            ],
+            "style": {
+              "color": "rgb(255, 203, 107)"
+            }
+          },
+          {
+            "types": [
+              "tag",
+              "deleted"
+            ],
+            "style": {
+              "color": "rgb(255, 85, 114)"
+            }
+          },
+          {
+            "types": [
+              "operator"
+            ],
+            "style": {
+              "color": "rgb(137, 221, 255)"
+            }
+          },
+          {
+            "types": [
+              "boolean"
+            ],
+            "style": {
+              "color": "rgb(255, 88, 116)"
+            }
+          },
+          {
+            "types": [
+              "keyword"
+            ],
+            "style": {
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "doctype"
+            ],
+            "style": {
+              "color": "rgb(199, 146, 234)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "namespace"
+            ],
+            "style": {
+              "color": "rgb(178, 204, 214)"
+            }
+          },
+          {
+            "types": [
+              "url"
+            ],
+            "style": {
+              "color": "rgb(221, 221, 221)"
+            }
+          }
+        ]
+      },
+      "additionalLanguages": []
+    },
+    "colorMode": {
+      "defaultMode": "light",
+      "disableSwitch": false,
+      "respectPrefersColorScheme": false,
+      "switchConfig": {
+        "darkIcon": "🌜",
+        "darkIconStyle": {},
+        "lightIcon": "🌞",
+        "lightIconStyle": {}
+      }
+    },
+    "docs": {
+      "versionPersistence": "localStorage"
+    },
+    "metadatas": [],
+    "hideableSidebar": false
+  },
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "C:\\Users\\skyle\\Projects\\ge-docs\\sidebars.js",
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/",
+          "routeBasePath": "/"
+        },
+        "theme": {
+          "customCss": "C:\\Users\\skyle\\Projects\\ge-docs\\src\\css\\custom.css"
+        }
+      }
+    ]
+  ],
+  "baseUrlIssueBanner": true,
+  "i18n": {
+    "defaultLocale": "en",
+    "locales": [
+      "en"
+    ],
+    "localeConfigs": {}
+  },
+  "onDuplicateRoutes": "warn",
+  "customFields": {},
+  "plugins": [],
+  "themes": [],
+  "titleDelimiter": "|",
+  "noIndex": false
+};
