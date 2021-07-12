3a4043e29ef7777af4b845945bbd772fdfa37287
Skylear <sky@surf.gg>
2021-07-12 11:40:42 -0500

    Fix accidental push

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\skyle\\Projects\\ge-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\skyle\\Projects\\ge-docs\\src\\css\\custom.css"),
-];
