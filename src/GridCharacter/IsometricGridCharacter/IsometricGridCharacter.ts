f02c8206348534cc14c5d5c3f8516dbd359639d9
Johannes Baum <baum@bw-webgestaltung.de>
2021-11-07 12:12:20 +0100

    #206 remove unused method

--- src/GridCharacter/IsometricGridCharacter/IsometricGridCharacter.ts
+++ src/GridCharacter/IsometricGridCharacter/IsometricGridCharacter.ts
@@ -1,13 +1,8 @@
-import { LayerPosition } from "./../../Pathfinding/ShortestPathAlgorithm";
 import { Direction, turnCounterClockwise } from "../../Direction/Direction";
 import { GridCharacter } from "../GridCharacter";
 
 export class IsometricGridCharacter extends GridCharacter {
   protected toMapDirection(direction: Direction): Direction {
     return turnCounterClockwise(direction);
-  }
-
-  protected mapDepth(nextTilePos: LayerPosition): number {
-    return nextTilePos.position.x + nextTilePos.position.y;
   }
 }
