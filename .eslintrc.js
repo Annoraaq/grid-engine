7307c8e7b54b88d22bb086e9bfdf915f723a13e6
Vildan Softic <zewa666@gmail.com>
2022-03-26 20:35:30 +0100

    chore: add eslint rule and parserOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,27 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
+  overrides: [
+    {
+      files: ['*.ts'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
   rules: {
     "@typescript-eslint/no-unused-vars": [
       "error",
       { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
     ],
     "@typescript-eslint/no-explicit-any": "off",
+    "require-await": "off",
+    "@typescript-eslint/require-await": "error",
     "@typescript-eslint/ban-ts-comment": "off",
     "no-empty-function": "off",
     "@typescript-eslint/no-empty-function": ["error", {
