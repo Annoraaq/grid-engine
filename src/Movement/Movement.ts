e6696fbb0c9f394a727841ba5b2f46e460d53edd
Johannes Baum <baum@bw-webgestaltung.de>
2022-06-26 18:01:11 +0200

    #107 add docs

--- src/Movement/Movement.ts
+++ src/Movement/Movement.ts
@@ -3,8 +3,13 @@ export interface Movement {
   getInfo(): MovementInfo;
 }
 
-// TODO docs
+/** Automatic movement information. */
 export interface MovementInfo {
+  /** Type of movement */
   type: string;
+  /**
+   * Configuration of movement. This is custom for every automatic movement
+   * type
+   */
   config?: Record<string, any>;
 }
