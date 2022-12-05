26c08430f379f7b84a0b84662f6d1c102f7a3982
Johannes Baum <annoraaq@users.noreply.github.com>
2022-12-05 19:31:58 +0100

    #270 add tsdocs

--- src/Pathfinding/Pathfinding.ts
+++ src/Pathfinding/Pathfinding.ts
@@ -17,19 +17,82 @@ import {
   ShortestPathAlgorithmType,
 } from "./ShortestPathAlgorithm";
 
+/**
+ * Configuration object for pathfinding.
+ */
 export interface PathfindingOptions {
+  /** Algorithm used for Pathfinding. */
   shortestPathAlgorithm?: ShortestPathAlgorithmType;
+
+  /**
+   * Consecutive tiles in x-dimension that need to fit in the path.
+   * This is useful for characters that span over multiple tiles.
+   */
+
   pathWidth?: number;
+  /**
+   * Consecutive tiles in y-dimension that need to fit in the path.
+   * This is useful for characters that span over multiple tiles.
+   */
   pathHeight?: number;
+
+  /**
+   * The number of directions to consider.
+   */
   numberOfDirections?: NumberOfDirections;
+
+  /**
+   * Function to specify whether a certain position is allowed for pathfinding.
+   * If the function returns false, the tile will be consindered as blocked.
+   *
+   * It can be used to restrict pathfinding to specific regions.
+   *
+   * Beware that this method can become a performance bottleneck easily. So be
+   * careful and keep it as efficient as possible. An asymptotic runtime
+   * complexity of O(1) is recommended.
+   */
   isPositionAllowed?: IsPositionAllowedFn;
+
+  /**
+   * The collision groups to consider for pathfinding.
+   */
   collisionGroups?: string[];
+
+  /**
+   * Set of characters to ignore at collision checking.
+   */
   ignoredChars?: CharId[];
+
+  /**
+   * If set to `true`, tile collisions will be ignored.
+   */
   ignoreTiles?: boolean;
+
+  /**
+   * If set to `true`, map boundaries are ignored. By default, positions
+   * outside of the boundaries of the tilemap are considered to be blocking,
+   * even if {@link PathfindingOptions.ignoreTiles} is set to `false`.
+   */
   ignoreMapBounds?: boolean;
+
+  /**
+   * If set to `true`, pathfinding will find a path, even if the target is
+   * blocked. This is a common use case if you want to find the shortest path
+   * to a blocking character.
+   */
   ignoreBlockedTarget?: boolean;
 }
 
+/**
+ * Function to specify whether a certain position is allowed for pathfinding.
+ * If the function returns false, the tile will be consindered as blocked.
+ *
+ * It can be used to restrict pathfinding to specific regions.
+ *
+ * Beware that this method can become a performance bottleneck easily. So be
+ * careful and keep it as efficient as possible. An asymptotic runtime
+ * complexity of O(1) is recommended.
+ */
 export type IsPositionAllowedFn = (
   pos: Position,
   charLayer?: string
