cf6b4c720b60b7df021c6505b3a14294c75f2fad
Johannes Baum <annoraaq@users.noreply.github.com>
2023-01-08 19:01:00 +0100

    Updates

---
+++ assets/js/page--node-modules--gridsome--app--pages--404-vue.bd288472.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{XtlX:function(n,t,u){"use strict";u.r(t);var l=u("KHd+"),o=Object(l.a)({},(function(){return(0,this._self._c)("h1",[this._v("404 - not found")])}),[],!1,null,null,null);t.default=o.exports}}]);
