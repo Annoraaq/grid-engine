f3e625e033838bdc9c0f4f5d8963c5aa92ae98c6
Johannes Baum <annoraaq@users.noreply.github.com>
2023-03-14 18:29:57 +0100

    #372 fix types dir

--- dist/main-esm.d.ts
+++
@@ -1,3 +0,0 @@
-import { GridEngine } from "./GridEngine";
-export { GridEngine, Direction, NoPathFoundStrategy, CollisionStrategy, Finished, FrameRow, MoveToConfig, MoveToResult, NumberOfDirections, PathBlockedStrategy, CharacterShiftAction } from "./GridEngine";
-export default GridEngine;
