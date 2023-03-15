5e0cf16483d7d5cf20cf32e9d6ccca18b57fdd21
Johannes Baum <annoraaq@users.noreply.github.com>
2023-03-15 18:33:59 +0100

    #373: use new mocks

--- src/Pathfinding/Bfs/Bfs.test.ts
+++ src/Pathfinding/Bfs/Bfs.test.ts
@@ -1,5 +1,5 @@
+import { createAllowedFn } from "../../Utils/MockFactory/MockFactory";
 import { Vector2 } from "../../Utils/Vector2/Vector2";
-import { VectorUtils } from "../../Utils/VectorUtils";
 import { LayerVecPos } from "../ShortestPathAlgorithm";
 import { Bfs } from "./Bfs";
 
@@ -91,36 +91,21 @@ describe("Bfs", () => {
   });
 
   it("should consider blocked tiles", () => {
-    // s = start
-    // t = target
-    // # = blocked
-    // . = free
-
-    // #####
-    // #s..#
-    // ###.#
-    // #t..#
-    // #####
+    const isBlocked = createAllowedFn([
+      "#####",
+      "#s..#",
+      "###.#",
+      "#t..#",
+      "#####",
+    ]);
 
     const startPos = { position: new Vector2(1, 1), layer: "layer1" };
     const targetPos = { position: new Vector2(1, 3), layer: "layer1" };
-    const unblockedTiles = [
-      VectorUtils.vec2str(new Vector2(1, 1)),
-      VectorUtils.vec2str(new Vector2(2, 1)),
-      VectorUtils.vec2str(new Vector2(3, 1)),
-      VectorUtils.vec2str(new Vector2(3, 2)),
-      VectorUtils.vec2str(new Vector2(3, 3)),
-      VectorUtils.vec2str(new Vector2(2, 3)),
-      VectorUtils.vec2str(new Vector2(1, 3)),
-    ];
 
     const { path, closestToTarget } = bfs.getShortestPath(
       startPos,
       targetPos,
-      (pos) =>
-        getNeighbors(pos).filter((n) =>
-          unblockedTiles.includes(VectorUtils.vec2str(n.position))
-        )
+      (pos) => getNeighbors(pos).filter((n) => isBlocked(n.position, n.layer))
     );
 
     expect(path).toEqual([
@@ -136,35 +121,21 @@ describe("Bfs", () => {
   });
 
   it("should return closest point if path is blocked", () => {
-    // s = start
-    // t = target
-    // # = blocked
-    // . = free
-
-    // #####
-    // #s..#
-    // #####
-    // #..t#
-    // #####
+    const isBlocked = createAllowedFn([
+      "#####",
+      "#s..#",
+      "#####",
+      "#..t#",
+      "#####",
+    ]);
 
     const startPos = { position: new Vector2(1, 1), layer: "layer1" };
     const targetPos = { position: new Vector2(3, 3), layer: "layer1" };
-    const unblockedTiles = [
-      VectorUtils.vec2str(new Vector2(1, 1)),
-      VectorUtils.vec2str(new Vector2(2, 1)),
-      VectorUtils.vec2str(new Vector2(3, 1)),
-      VectorUtils.vec2str(new Vector2(3, 3)),
-      VectorUtils.vec2str(new Vector2(2, 3)),
-      VectorUtils.vec2str(new Vector2(1, 3)),
-    ];
 
     const { path, closestToTarget } = bfs.getShortestPath(
       startPos,
       targetPos,
-      (pos) =>
-        getNeighbors(pos).filter((n) =>
-          unblockedTiles.includes(VectorUtils.vec2str(n.position))
-        )
+      (pos) => getNeighbors(pos).filter((n) => isBlocked(n.position, n.layer))
     );
 
     expect(path).toEqual([]);
