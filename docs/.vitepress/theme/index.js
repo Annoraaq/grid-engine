2daf6bf5fabc48f927f991aeee4795740b7675da
Johannes Baum <annoraaq@users.noreply.github.com>
2023-07-06 19:22:43 +0200

    #403 clean up theme

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -12,9 +12,7 @@ export default {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
       'nav-bar-title-before': () => h(Logo),
-      // 'doc-top': () => h(Logo),
       'home-hero-image': () => h(Grid)
-      // 'home-hero-image': () => h(Logo)
     })
   },
   enhanceApp({ app, router, siteData }) {
