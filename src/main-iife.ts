d55762b337f7f5bc614f10bd5fda9103657d9ead
Johannes Baum <annoraaq@users.noreply.github.com>
2023-07-26 19:27:00 +0200

    #425 fix iife

--- src/main-iife.ts
+++ src/main-iife.ts
@@ -1,7 +1,6 @@
 import * as ge from "./GridEngine";
 
-export default ge.GridEngine;
 // @ts-ignore
 window.GridEngine = ge.GridEngine;
 // @ts-ignore
-window.GridEngineImports = ge;
+window.GridEngineImports = ge.GridEngine;
