8f3f4e2ef339acf3c06a7ce3f2438749391d7009
Johannes Baum <annoraaq@users.noreply.github.com>
2026-07-27 20:19:17 +0200

    #564 optimize transitions

---
+++ dist/mjs/speedtests/tests/Jps8Speed.d.ts
@@ -0,0 +1,8 @@
+import { GridEngineHeadless } from "../../src/GridEngineHeadless.js";
+export declare const Jps8Speed: {
+    name: string;
+    run: (gridEngine: GridEngineHeadless) => {
+        result: number;
+        tolerance: number;
+    };
+};
